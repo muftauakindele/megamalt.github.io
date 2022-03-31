8318c7b2ae65b5b00f07323fffb0b90e1619571e
Muftau Akindele <dayoakindele@gmail.com>
2022-03-30 20:00:35 -0500

    1.1

--- public/index.js
+++ public/index.js
@@ -4,7 +4,7 @@ document.body.className = "visible";
 // When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
-  var currentScrollPos = window.pageYOffset;
+  let currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
   } else {
@@ -46,3 +46,63 @@ allLinks.forEach(function (link) {
     }
   });
 });
+/////////////////////////////////////////////////////////
+//Palm Tree Animation
+// $(document).on("ready", function () {
+//   $(window).on("scroll", function () {
+//     $(".palm-tree").each(function (i) {
+//       let bottom_of_object = $(this).offset().top + $(this).outerHeight();
+//       let bottom_of_window = $(window).scrollTop() + $(window).height();
+//       if (bottom_of_window > bottom_of_object) {
+//         $(this).addClass(".fromRight");
+//       }
+//       if (bottom_of_window < bottom_of_object) {
+//         $(this).removeClass(".fromRight");
+//       }
+//     });
+//   });
+// });
+
+// const checkpoint = 300;
+
+// window.addEventListener("scroll", () => {
+//   const currentScroll = window.pageYOffset;
+//   if (currentScroll <= checkpoint) {
+//     opacity = 1 - currentScroll / checkpoint;
+//   } else {
+//     opacity = 0;
+//   }
+//   document.querySelector(".fromRight").style.opacity = opacity;
+// });
+
+//Faders
+// const faders = document.querySelectorAll(".fadeIn");
+// const sliders = document.querySelectorAll(".slideIn");
+
+// const appearOptions = {
+//   threshold: 0,
+//   rootMargin: "0px 0px -65px 0px",
+// };
+
+// const appearOnScroll = new IntersectionObserver(function (
+//   entries,
+//   appearOnScroll
+// ) {
+//   entries.forEach((entry) => {
+//     if (!entry.isIntersecting) {
+//       return;
+//     } else {
+//       entry.target.classList.add("appear");
+//       appearOnScroll.unobserve(entry.target);
+//     }
+//   });
+// },
+// appearOptions);
+
+// faders.forEach((fader) => {
+//   appearOnScroll.observe(fader);
+// });
+
+// sliders.forEach((slider) => {
+//   appearOnScroll.observe(slider);
+// });
