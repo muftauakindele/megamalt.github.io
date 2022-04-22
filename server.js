6e65d86921aba6cebb2fbe5b50bccb0a7cb0b039
Muftau Akindele <dayoakindele@gmail.com>
2022-04-22 03:18:14 -0500

    quieries

--- server.js
+++ server.js
@@ -6,19 +6,20 @@ const path = require("path");
 const app = express();
 const { urlencoded } = require("body-parser");
 const { sendfile } = require("express/lib/response");
+const { dirname } = require("path");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(__dirname + "index.js"));
 
 app.get("/", (req, res) => {
-  res.sendFile("./public/index.html", { root: __dirname });
+  res.sendFile(__dirname + "/public/index.html");
 });
 app.get("/about", (req, res) => {
-  res.sendFile("./public/about.html", { root: __dirname });
+  res.sendFile(__dirname + "/public/about.html");
 });
 app.get("/distributor", (req, res) => {
-  res.sendFile("./public/distributor.html", { root: __dirname });
+  res.sendFile(__dirname + "/public/distributor.html");
 });
 
 app.listen(process.env.PORT || 3000, () =>
